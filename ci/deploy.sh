e3b987673c0441f33f00a6153f596d21f1a5faba
Anthony Dahanne <anthony.dahanne@gmail.com>
2023-12-12 20:08:12 -0500

    Add pom metadata and script for central * since repo.spring.io is no longer an option

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,5 +4,12 @@ set -euo pipefail
 
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 
+export REPOSITORY="${PWD}"/repository
+
 cd java-buildpack-client-certificate-mapper
-./mvnw -q -Dmaven.test.skip=true deploy
+./mvnw -Dmaven.test.skip=true deploy -DcreateChecksum=true -DaltDeploymentRepository="local::default::file://${REPOSITORY}"
+
+echo "here are the published jars in ${REPOSITORY}"
+ls -al ${REPOSITORY}/org/cloudfoundry/
+ls -al ${REPOSITORY}/org/cloudfoundry/java-buildpack-client-certificate-mapper/
+ls -al ${REPOSITORY}/org/cloudfoundry/java-buildpack-client-certificate-mapper/2.0.0
