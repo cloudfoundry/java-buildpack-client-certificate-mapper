2ab9ae89b4141ef2ab4a88a604e1e4f6faf86d93
Anthony Dahanne <anthony.dahanne@gmail.com>
2023-12-13 21:42:02 -0500

    v2.0.1-SNAPSHOT Development

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -9,7 +9,3 @@ export REPOSITORY="${PWD}"/repository
 cd java-buildpack-client-certificate-mapper
 ./mvnw -Dmaven.test.skip=true deploy -DcreateChecksum=true -DaltDeploymentRepository="local::default::file://${REPOSITORY}"
 
-echo "here are the published jars in ${REPOSITORY}"
-ls -al ${REPOSITORY}/org/cloudfoundry/
-ls -al ${REPOSITORY}/org/cloudfoundry/java-buildpack-client-certificate-mapper/
-ls -al ${REPOSITORY}/org/cloudfoundry/java-buildpack-client-certificate-mapper/2.0.0
